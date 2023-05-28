a1270fff0726b15a5fcb876c63607a83a0794579
valeria caliguire <caliguireval@gmail.com>
2023-05-28 15:21:55 -0600

    must redo

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,20 @@
 
 from user import User
 
+
+knowledge = []
+
+
 class Student(User):
+    knowledge = []
+
+   # def __init__(self, knowledge):
+     #   print("Student.__init__ called.")
+     #   super().__init__(knowledge)
+     #   self.knowledge
     
-    def learn(self):
-        pass
+    def learn(self, knowledge):
+        self.knowledge = knowledge
+        
+    #    self.knowledge.append(element)
+    #    return self.knowledge
